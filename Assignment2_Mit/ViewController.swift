1808dea8ef1e8bb7024d5d3967bba8eb75b07b67
Mit <mitkpatel09@gmail.com>
2021-10-25 13:47:26 -0400

    Initial Commit

---
+++ Assignment2_Mit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2_Mit
+//
+//  Created by user202387 on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assignment2_MitTests/Assignment2_MitTests.swift
@@ -0,0 +1,33 @@
+//
+//  Assignment2_MitTests.swift
+//  Assignment2_MitTests
+//
+//  Created by user202387 on 10/25/21.
+//
+
+import XCTest
+@testable import Assignment2_Mit
+
+class Assignment2_MitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
